1936c321565bb97a1e082e71e8a0013b7f2dad87
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-04-09 14:03:00 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,10 @@
+const missionImpossiblePoster = [
+    "url(./images/movies/genre/mission-impossible/mission-impossible-one.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-two.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-three.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-four.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-five.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-six.jpg)",
+    "url(./images/movies/genre/mission-impossible/mission-impossible-seven.jpg)",
+]
+
