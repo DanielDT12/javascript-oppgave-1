aa040a92c383d1a5d086142e75d28ccea4050f2e
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-04-10 12:59:23 +0200

    lagt til test datasett

---
+++ script/modules/movie-database.js
@@ -0,0 +1,70 @@
+export const pageDocs = {
+    missionImpossible: document.querySelector("#mission-impossible-series")
+}
+
+
+export const movieReview = {
+    movie1: {
+        title: "Mission Impossible",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-one.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie2: {
+        title: "Mission Impossible 2",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-two.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie3: {
+        title: "Mission Impossible 3",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-three.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie4: {
+        title: "Mission Impossible 4",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-four.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie5: {
+        title: "Mission Impossible 5",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-five.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie6: {
+        title: "Mission Impossible 6",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-six.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+    movie7: {
+        title: "Mission Impossible 7",
+        genre: "action",
+        imdbRating: 7.2,
+        moviePoster: "../images/movies/genre/action/mission-impossible/mission-impossible-seven.jpg",
+        releaseDate: 1996,
+        ageRating: 15,
+        runTime: "1h50m"
+    },
+}
