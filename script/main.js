685886894ec4e54d9a12ada8792cbd17e9a5276d
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-04-12 10:16:46 +0200

    endret på funksjon så den looper igjennom movie-database

--- script/main.js
+++ script/main.js
@@ -2,32 +2,27 @@ import { movieReview, pageDocs } from "./modules/movie-database.js"
 
 movieCard()
 
-
 function movieCard() {
-    for (let i = 0; i < 7; i++) {
+    pageDocs.missionImpossible.innerHTML = '';
+    for (let i = 1; i <= Object.keys(movieReview).length; i++) {
+        const movie = movieReview[`movie${i}`]; 
         pageDocs.missionImpossible.innerHTML += `
             <div class="movie-card flex-row">
-                <img src="${movieReview.movie1.moviePoster}" class="movie-poster" alt="Mission Impossible">
+                <img src="${movie.moviePoster}" class="movie-poster" alt="${movie.title}" />
                 <div class="movie-info flex-column space-between">
-                    <h3 class="movie-card__title">${movieReview.movie1.title}</h3>
+                    <h3 class="movie-card__title">${movie.title}</h3>
                     <div class="flex-row space-between">
-                        <p>Imdb Rating: ${movieReview.movie1.imdbRating}</p>
-                        <p>Genre: ${movieReview.movie1.genre}</p>
+                        <p>Imdb Rating: ${movie.imdbRating}</p>
+                        <p>Genre: ${movie.genre}</p>
                     </div>
                     <div class="flex-row space-between">
-                        <p>Release date: ${movieReview.movie1.releaseDate}</p>
-                        <p>Age rating: ${movieReview.movie1.ageRating}</p>
+                        <p>Release date: ${movie.releaseDate}</p>
+                        <p>Age rating: ${movie.ageRating}</p>
                     </div>
                     <div>
-                        <p>Runtime: ${movieReview.movie1.runTime}</p>
+                        <p>Runtime: ${movie.runTime}</p>
                     </div>
                 </div>
-            </div>`
-        };
-    
+            </div>`;
+    }
 };
-
-
-
-console.log(pageDocs)
-
